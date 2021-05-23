e86a1e0b60549a4aae61ea54691ddcf18b70de4b
Daniel Heise <daniel.heise@5minds.de>
2021-05-23 17:32:56 +0200

    Add external task handler application

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # 5Minds Process Engine with SQL Server basic setup
-`docker-compose` setup for running the 5Minds Process Engine with MS SQL Server.
 
-Run `docker-compose up -d` to starte the setup.
+This setup contains a simple external task handler application written in C#/.NET 5.
+
+Use `docker-compose build` to build an local image for the externaltaskhandler application.
+
+Run `docker-compose up -d` to start the setup.
---
+++ externaltaskhandler/Program.cs
@@ -0,0 +1,38 @@
+﻿using AtlasEngine;
+using AtlasEngine.ExternalTasks;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Threading.Tasks;
+
+var configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .AddEnvironmentVariables()
+    .AddCommandLine(args)
+    .Build();
+
+var logger = LoggerFactory
+    .Create(builder => builder.AddConfiguration(configuration.GetSection("Logging")).AddConsole())
+    .CreateLogger("externaltaskhandler");
+
+var externalTaskClient = ClientFactory.CreateExternalTaskClient(new Uri(configuration.GetValue<string>("ProcessEngineUri")));
+
+logger.LogInformation("Start external tasks handling...");
+
+var worker = externalTaskClient.SubscribeToExternalTaskTopic(
+    "SampleExternalTask",
+    s => s.UseHandlerMethod((ExternalTaskPayload payload, ExternalTask task) =>
+    {
+        logger.LogTrace($@"Handling external task {task.Topic} for process instance {task.ProcessInstanceId}{Environment.NewLine}
+                        External task data: '{payload.Data}'");
+        return Task.FromResult(new ExternalTaskResponse());
+    }));
+
+logger.LogInformation("External task handling started.");
+logger.LogInformation("Press CTRL+C to exit");
+
+await worker.StartAsync();
+
+record ExternalTaskPayload(string Data);
+
+record ExternalTaskResponse();
